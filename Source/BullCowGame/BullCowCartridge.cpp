071471b44164affa03dd705b3badf1f93114739f
Shimshon <shimpolak@gmail.com>
2019-10-28 08:09:45 +0200

    Pesudo code added

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -4,25 +4,34 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
-	PrintLine(TEXT("Mooorning player,welcom to Bull & Cow game. "));
-	PrintLine(TEXT("Gauss the X numbers"));
+	PrintLine(TEXT("Moorning player,welcome to Bull & Cow game."));
+	PrintLine(TEXT("Guess the X numbers"));
+	//Set the player lives number
+	PlayerLives = 4;
 	PrintLine(TEXT("Press 'Enter' button to continue..."));
+	HiddenWord = TEXT("car");
+	//Ask from the player to gauess a word
+
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
-
 	ClearScreen();
-	FString HiddenWord = TEXT("car");
+
 	FString PlayerGauss = Input;
-	if (HiddenWord == PlayerGauss)
-	{
+
+	if (HiddenWord == PlayerGauss)// Check the player word
+	{//If correct the player win and should be asked if he want to play again
 		PrintLine("You have won!");
 	}
-	else {
-		PrintLine("You have lost!");
+	else {//If the player wrong one life is subbtrect
+		if (PlayerLives == 0) {
+			PrintLine("You have lost!");
+			//Ask the player to play again
+		}
+		else {
+			PrintLine("Wrong answer try again!");
+			PlayerLives--;
+		}
 	}
-/*	while (HiddenWord != PlayerGauss) {
-
-	}*/
 }
